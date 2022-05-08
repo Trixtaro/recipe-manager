aae778d45d81aa5f9111805389ae65f7e4c39f82
Trixtaro <wilmerdavid97@hotmail.com>
2022-05-07 22:00:24 -0500

    Adding tailwindcss to the project

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <button>Agregar nuevo ingrediente</button>
       <button>Actualizar inventario</button>
       <button>Agregar receta</button>
